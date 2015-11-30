37be6299651c2ee931af3ff5edef8eb3a52bbab2
cheeks <joshkanner@Joshuas-MacBook-Pro.local>
2015-11-30 12:31:44 -0800

    added router, broke out pages

---
+++ src/js/resume.js
@@ -0,0 +1,20 @@
+let React = require('react');
+
+
+class ResumePage extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <h1>Joshua Kanner</h1>
+            <h2>Blah Blah Blah</h2>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+module.exports = ResumePage;
